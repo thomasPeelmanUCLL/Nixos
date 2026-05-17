152329543e3a6ab1ffab0bf007c5bee35e3d7ead
thomasPeelmanUCLL <thomas.peelman7777@gmail.com>
2026-05-17 16:56:12 +0200

    nvidia

--- nvidia.nix
+++ nvidia.nix
@@ -2,20 +2,17 @@
 { config, pkgs, ... }:
 
 {
-  hardware.opengl = {
-  enable = true;
-  driSupport = true;
-  driSupport32Bit = true;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
   };
 
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia.modesetting.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
 
-  hardware.nvidia.prime = {
-    sync.enable = true;
-
-    # dedicated
-    nvidiaBusId = "PCI:2b:0:0";
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
-
 }
