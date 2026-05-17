1b4acce68206ef6c25cea90196444db4f441f5e5
thomasPeelmanUCLL <thomas.peelman7777@gmail.com>
2026-05-17 18:37:22 +0200

    it finnaly works

--- flake.nix
+++ flake.nix
@@ -4,38 +4,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
-    nix-citizen = {
-      url = "github:LovingMelody/nix-citizen";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nix-citizen.url = "github:LovingMelody/nix-citizen";
   };
 
-  outputs = { self, nixpkgs, nix-citizen, ... }:
+  outputs = { self, nixpkgs, nix-citizen, ... }@inputs:
     let
       system = "x86_64-linux";
     in {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         inherit system;
 
-        specialArgs = {
-          inherit nix-citizen;
-        };
+        specialArgs = { inherit inputs system; };
 
         modules = [
           ./configuration.nix
-          {
-            nix.settings = {
-              experimental-features = [ "nix-command" "flakes" ];
-              substituters = [
-                "https://cache.nixos.org/"
-                "https://nix-citizen.cachix.org"
-              ];
-              trusted-public-keys = [
-                "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-                "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="
-              ];
-            };
-          }
         ];
       };
     };
--- gaming.nix
+++ gaming.nix
@@ -1,5 +1,5 @@
 # gaming.nix
-{ pkgs, nix-citizen, ... }:
+{ pkgs, inputs,  system,  ... }:
 
 {
   programs.steam = {
@@ -7,7 +7,21 @@
     remotePlay.openFirewall = true;
   };
 
+  boot.kernel.sysctl = {
+    "vm.max_map_count" = 1048576;
+    "fs.file-max" = 524288;
+  };
+
+  nix.settings = {
+    substituters = [ "https://nix-citizen.cachix.org" ];
+    trusted-public-keys = [
+      "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="
+    ];
+  };
+
   environment.systemPackages = with pkgs; [
+   inputs.nix-citizen.packages.${system}.rsi-launcher
+    wget
     rnnoise-plugin
     git
     mesa-demos
@@ -15,11 +29,9 @@
     pciutils
     lm_sensors
     vim
-    wget
     baobab
     nil
     nixfmt-rfc-style
     fastfetch
-    #nix-citizen.packages.x86_64-linux.rsi-launcher
   ];
 }
