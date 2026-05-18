9d0bef3794dfafc97504c55677ec9b97e7be9228
pants <145909640+thomasPeelmanUCLL@users.noreply.github.com>
2026-05-18 15:35:44 +0200

    Add NVIDIA kernel module and disable ASPM

--- configuration.nix
+++ configuration.nix
@@ -19,6 +19,8 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelModules= [ "nvidia"];
+  boot.kernelParams=["pcie_aspm=off"];
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
