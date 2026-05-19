341f2259443d222e63f414d35ca806a95c94866e
pants <145909640+thomasPeelmanUCLL@users.noreply.github.com>
2026-05-20 00:46:58 +0200

    chore: remove old root-level nix files (replaced by modules/)

--- nvidia.nix
+++ nvidia.nix
@@ -1,18 +1,2 @@
-# nvidia.nix
-{ config, pkgs, ... }:
-
-{
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
-  };
-}
+# Moved to modules/features/nvidia.nix
+# Safe to delete.
--- users/bob.nix
+++ users/bob.nix
@@ -1,20 +1,2 @@
-# users/bob.nix
-{ pkgs, ... }:
-
-{
-  users.users.bob = {
-    isNormalUser = true;
-    description = "bob";
-    extraGroups = [ "networkmanager" "wheel" "video"];
-    packages = with pkgs; [
-      kdePackages.kate
-      thunderbird
-      brave
-      discord-ptb
-      discord
-      pavucontrol
-      vscode
-      blender
-    ];
-  };
-}
+# Moved to modules/users/bob.nix
+# Safe to delete.
